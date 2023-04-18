7b5cdf44c24028b60ed20ef5bb23d69fd8e61d99
sabirabanu94 <128887202+sabirabanu94@users.noreply.github.com>
2023-04-18 13:28:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let resume = {
+    "basics": {
+        "name": "Sabira Banu M",
+        "email": "sabirabanu94@gmail.com",
+        "phone": 9677143649,
+        "degree": "B.E",
+        "location": {
+            "address": "No 3 Sait Colony 2nd Street, Egmore",
+            "postalCode": 600008,
+            "city": "Chennai",
+            "state": "Tamilnadu",
+            "country": "India"
+        },
+        "profiles": [{
+            "website": "https://www.linkedin.com/in/sabira-banu-m-82a330a0/",
+            "github": "https://github.com/sabirabanu94"
+        }]
+    },
+    "work": [{
+        "company": "TNQ Technologies",
+        "position": "Analyst-Support",
+        "startDate": "2020-03-11",
+        "endDate": "Till Now",
+        "summary": "To resolve customers queries using MYSQL",
+    }, ],
+    "education": [{
+        "institution": "AMS college of Engineering",
+        "department": "Electronics and Communication Engineering",
+        "studyType": "fulltime",
+        "batch start year": 2013,
+        "batch end year": 2016,
+        "gpa": 6.8,
+    }],
+    "skills": [{
+        "name": "python,javascript",
+        "level": "beginer",
+    }],
+    "languages": [{
+        "language": "Tamil,Enlish",
+    }],
+}
+
+console.log(resume);
